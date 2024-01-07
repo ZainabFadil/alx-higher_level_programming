cb68cc4efb56abeace3eb9166928eedc43588fb3
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-01-07 11:40:44 +0200

    last edition

--- 0x01-python-if_else_loops_functions/9-main.py
+++ 0x01-python-if_else_loops_functions/9-main.py
@@ -5,4 +5,4 @@ print_last_digit = __import__('9-print_last_digit').print_last_digit
 
 # print_last_digit(69)
 
-print_last_digit(98)
+print_last_digit(-98)
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    flag = 0
     num = abs(number) % 10
+    x = num
     if (number) < 0:
         num *= -1
+        flag = 1
     print("{}".format(num), end="")
-    return (num)
+    return (x)
+
