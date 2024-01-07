f6319303b957eaa99803479d18b47ad1cd2c737c
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-01-07 11:51:40 +0200

    last edit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    #flag = 0
     num = abs(number) % 10
-    # x = num
-    # if (number) < 0:
-    #     num *= -1
-    #     flag = 1
     print("{}".format(num), end="")
     return (num)
-
