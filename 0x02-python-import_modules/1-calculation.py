f3db4f198453adc66dc1ff33814f9de42a57c70a
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-01-10 07:58:09 +0200

    calc importing

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
+if __name__ == "__main__":
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
 a = 10
 b = 5
 print("{} + {} = {}".format(a, b, add(a, b)))
