7db363fd6e5eccdfafdd73f55e48e1ab20d4dafb
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-01-11 10:40:22 +0200

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+length = len(sys.argv)
+length -= 1
+if (length == 0):
+    print("0 arguments.")
+elif (length == 1):
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:".format(length))
+    for i in range(1, length + 1):
+        print("{}: {}".format(i, sys.argv[i]))
