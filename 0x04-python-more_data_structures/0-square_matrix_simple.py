6a8ce955dccc5321b30d7a97da4541b92d564811
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-01-22 10:01:57 +0200

    square matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    newMatrix = []
+    for row in matrix:
+        newRow = []
+        for element in row:
+            newRow.append(element ** 2)
+        newMatrix.append(newRow)
+    return newMatrix
