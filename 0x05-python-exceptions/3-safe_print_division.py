9dcdc040673bddcd747b887be003e34e70106079
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-01-24 20:14:17 +0200

    division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         x = a / b
         ret = x
-    except ZeroDivisionError:
+    except (ZeroDivisionError, TypeError):
         ret = None
     finally:
         print("Inside result: {}".format(ret))
