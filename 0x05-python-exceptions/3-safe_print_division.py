0ba79fe7b6803a2ab6606cc5cf010c1cb9c53d08
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-01-24 20:09:13 +0200

    division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    ret = 1
+    try:
+        x = a / b
+        ret = x
+    except ZeroDivisionError:
+        ret = None
+    finally:
+        print("Inside result: {}".format(ret))
+        if ret:
+            return x
+        else:
+            return None
