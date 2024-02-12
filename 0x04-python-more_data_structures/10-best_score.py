7698f5e020f8fdcef74d16f40dc5a5783d690d75
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-02-12 19:55:27 +0200

    my commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,4 +6,5 @@ def best_score(a_dictionary):
     for i in a_dictionary.keys():
         if a_dictionary[i] > maxm:
             maxm = a_dictionary[i]
-    return maxm
+            ret = i
+    return ret
