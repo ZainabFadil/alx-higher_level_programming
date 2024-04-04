b72d8db8968d6ef453f99b7a5cee737b0489101d
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-04 16:09:10 +0200

    delete all

--- 0x01-python-if_else_loops_functions/7-islower.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def islower(c):
-    if (ord(c) >= ord('a') or ord(c) >= ord('z')):
-        return True
-    return False
--- 0x01-python-if_else_loops_functions/9-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-# print_last_digit(-20)
-
-# print_last_digit(69)
-
-print_last_digit(-98)
