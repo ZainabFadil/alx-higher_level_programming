74dfc7e3e91bf373e099078fadea1216fd615b36
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-15 10:51:53 +0200

    my commit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        my_tuple = None, ""
-        return my_tuple
-    my_tuple = len(sentence), sentence[0]
-    return my_tuple
+    if not sentence:
+        return 0, None
+    return len(sentence), sentence[0]
