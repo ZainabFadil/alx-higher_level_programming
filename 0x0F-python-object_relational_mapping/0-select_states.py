2afb6338b5cfbae9d76bf093e8568e617bbd6b3c
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-15 04:32:33 +0200

    my commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+lists scripts from database
 """
 
 import MySQLdb
@@ -9,8 +8,7 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the states
-    from the database.
+    Acess database
     """
     db_connect = MySQLdb.connect(
         host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
