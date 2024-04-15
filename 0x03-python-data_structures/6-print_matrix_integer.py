678a0a02c3e76fbf12af18d76d082dfa120c5232
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-15 10:35:46 +0200

    my commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
         for pos in range(len(rows)):
             if pos != len(rows) - 1:
-                print("{}".format(rows[pos]), end=" ")
+                print("{:d}".format(rows[pos]), end=" ")
             else:
-                print("{}".format(rows[pos]), end="")
+                print("{:d}".format(rows[pos]), end="")
         print("")
