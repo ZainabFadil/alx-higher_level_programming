d4408a79198cbba2a36de534e1cf15b23cf995fa
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-24 11:24:47 +0200

    last commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    flag = 1
     myDcit = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    n = len(roman_string)
+    i = n - 1
     sum = 0
-    for i in range (len(roman_string)):
-        if not flag:
-            flag = 1
-            continue
-
-        sum += myDcit[roman_string[i]]
-        
-        if roman_string[i] == "I":
-            if roman_string[i+1] == "X" or roman_string[i+1] == "V":
-                sum += myDcit[roman_string[i+1]]
-                sum -= 1
-                flag = 0
+    while i >= 0:
+        if i < n -1 and myDcit[roman_string[i]] < myDcit[roman_string[i+1]]:
+            sum -= myDcit[roman_string[i]]
+        else:
+            sum += myDcit[roman_string[i]]
+        i -= 1
     return sum
-print (roman_to_int("XI"))
