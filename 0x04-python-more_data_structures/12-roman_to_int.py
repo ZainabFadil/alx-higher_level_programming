4d328d92bb4c2dde43d78af6bbdad1c72e0376cc
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-24 11:29:59 +0200

    last commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,6 @@
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
-
     myDcit = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     n = len(roman_string)
     i = n - 1
