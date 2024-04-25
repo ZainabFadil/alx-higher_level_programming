c614c37d6c2118fbbd80e8d0728fafda8627f3b3
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-25 23:00:50 +0200

    my commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module for deining square"""
+
+
+class Square:
+    """class for squares"""
+    def __init__(self, size):
+        """
+        constructor to initialize values
+        Args:
+            size(int): size of the squar
+        """
+        self.__size = size
+
+
+square1 = Square(5)
