744a9ec03265f8d188c0e463b3982d992fece7db
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-26 19:51:06 +0300

    my commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,8 +10,9 @@ class Square:
         Args:
             size(int): size of the squar
         """
-        if size <= 0:
-            raise ValueError("size must be positive")
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
         self.__size = size
