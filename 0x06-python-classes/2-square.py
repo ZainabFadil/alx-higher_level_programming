94723a719a4ed2db74f778f8c27d763cca0cbc5d
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-26 19:24:51 +0300

    my commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module for deining square"""
+
+
+class Square:
+    """class for squares"""
+    def __init__(self, size = 0):
+        """
+        constructor to initialize values
+        Args:
+            size(int): size of the squar
+        """
+        assert size >= 0, ValueError('size must be >= 0')
+        assert type(size) is int, TypeError('size must be >= 0')
+        self.__size = size
+
+
+square1 = Square(5)
