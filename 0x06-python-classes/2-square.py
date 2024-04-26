bf5bc83ab7b61e158bdbb30e9406be93345bc038
ZainabFadil <ugs.19687@ci.suez.edu.eg>
2024-04-26 19:26:16 +0300

    my commit

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """class for squares"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """
         constructor to initialize values
         Args:
